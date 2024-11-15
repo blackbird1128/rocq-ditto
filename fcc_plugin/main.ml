827ae9dc7a3fc8e59eaa4253184a680da1a67c79
blackbird1128 <37084688+blackbird1128@users.noreply.github.com>
2024-11-15 09:57:42 +0100

    continue switching to an fcc plugin with an associated lib

--- fcc_plugin/main.ml
+++ fcc_plugin/main.ml
@@ -1,17 +1,5 @@
 open Fleche
-
-let pp_json fmt (ast : Doc.Node.Ast.t) =
-  Lsp.JCoq.Ast.to_yojson ast.v |> Yojson.Safe.pretty_print fmt
-
-let pp_sexp fmt (ast : Doc.Node.Ast.t) =
-  Serlib.Ser_vernacexpr.sexp_of_vernac_control (Coq.Ast.to_coq ast.v)
-  |> Sexplib.Sexp.pp_hum fmt
-
-let pw pp fmt ast = Format.fprintf fmt "@[%a@]" pp ast
-
-let dump_asts ~out_file pp asts =
-  let f fmt asts = List.iter (pw pp fmt) asts in
-  Coq.Compat.format_to_file ~file:out_file ~f asts
+open Ditto
 
 let dump_ast ~io ~token:_ ~(doc : Doc.t) =
   let uri = doc.uri in
@@ -19,12 +7,20 @@ let dump_ast ~io ~token:_ ~(doc : Doc.t) =
   let lvl = Io.Level.Info in
   Io.Report.msg ~io ~lvl "[ast plugin] dumping ast for %s ..." uri_str;
   let asts = Doc.asts doc in
+  let coq_asts =
+    List.map (fun (x : Doc.Node.Ast.t) -> Coq.Ast.to_coq x.v) asts
+  in
+
+  List.iter
+    (fun x -> Ppvernac.pr_vernac x |> Pp.string_of_ppcmds |> print_endline)
+    coq_asts;
+  (* (fun x -> *)
+  (*   Ppvernac.pr_vernac (Coq.Ast.to_coq *)
+  (*   |> Pp.string_of_ppcmds |> print_endline) *)
+  (*   asts; *)
   (* Output json *)
   let out_file_j = Lang.LUri.File.to_string_file uri ^ ".json.astdump" in
-  dump_asts ~out_file:out_file_j pp_json asts;
-  (* Output sexp *)
-  let out_file_s = Lang.LUri.File.to_string_file uri ^ ".sexp.astdump" in
-  dump_asts ~out_file:out_file_s pp_sexp asts;
+
   Io.Report.msg ~io ~lvl "[ast plugin] dumping ast for %s was completed!"
     uri_str;
   ()
