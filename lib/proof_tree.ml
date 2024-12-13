911f296140c1a612eb0623506de738d4fa72a30c
alexJ <37084688+blackbird1128@users.noreply.github.com>
2024-12-13 15:28:40 +0100

    add a way to pretty print and compare nary_tree

--- lib/proof_tree.ml
+++ lib/proof_tree.ml
@@ -1,5 +1,13 @@
 type 'a nary_tree = Node of 'a * 'a nary_tree list
 
+let rec equal_nary_tree equal_a (Node (a1, children1)) (Node (a2, children2)) =
+  equal_a a1 a2 && List.for_all2 (equal_nary_tree equal_a) children1 children2
+
+let rec pp_nary_tree pp_a fmt (Node (a, children)) =
+  Format.fprintf fmt "Node (%a, [%a])"
+    pp_a a
+    (Format.pp_print_list (pp_nary_tree pp_a)) children
+
 let rec flatten_filter f (Node (x, children)) : 'a nary_tree list =
   let processed_children = List.concat_map (flatten_filter f) children in
   if f x then
--- lib/proof_tree.mli
+++ lib/proof_tree.mli
@@ -1,3 +1,18 @@
 type 'a nary_tree = Node of 'a * 'a nary_tree list
 
 val remove_all_nonmatching : ('a -> bool) -> 'a nary_tree -> 'a nary_tree option
+
+(** Compare two n-ary trees for equality. *)
+(** 
+  [equal_nary_tree equal_a tree1 tree2] returns [true] if [tree1] and [tree2]
+  are structurally equal and their elements satisfy [equal_a].
+*)
+val equal_nary_tree : 
+  ('a -> 'a -> bool) -> 'a nary_tree -> 'a nary_tree -> bool
+
+(** Pretty-printer for n-ary trees. 
+  [pp_nary_tree pp_a fmt tree] prints the [tree] to the formatter [fmt]
+  using [pp_a] to print elements of type ['a].
+ *)
+val pp_nary_tree : 
+  (Format.formatter -> 'a -> unit) -> Format.formatter -> 'a nary_tree -> unit
