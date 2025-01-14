9a92d7520fec61a6c35a874ff3d99aaa40b7e747
alexJ <37084688+blackbird1128@users.noreply.github.com>
2025-01-14 23:57:12 +0100

    Add documentation to proof_tree module

--- lib/proof_tree.ml
+++ lib/proof_tree.ml
@@ -1,11 +1,13 @@
 type 'a nary_tree = Node of 'a * 'a nary_tree list
 
-let rec equal_nary_tree equal_a (Node (a1, children1)) (Node (a2, children2)) =
+let rec equal_nary_tree (equal_a : 'a -> 'a -> bool) (Node (a1, children1))
+    (Node (a2, children2)) : bool =
   try
     equal_a a1 a2 && List.for_all2 (equal_nary_tree equal_a) children1 children2
   with Invalid_argument _ -> false
 
-let rec pp_nary_tree pp_a fmt (Node (a, children)) =
+let rec pp_nary_tree (pp_a : Format.formatter -> 'a -> unit)
+    (fmt : Format.formatter) (Node (a, children)) : unit =
   (* Print the current node value *)
   Format.fprintf fmt "%a" pp_a a;
   (* If the node has children, print them in parentheses *)
@@ -19,7 +21,8 @@ let rec pp_nary_tree pp_a fmt (Node (a, children)) =
       children;
     Format.fprintf fmt ")")
 
-let rec flatten_filter f (Node (x, children)) : 'a nary_tree list =
+let rec flatten_filter (f : 'a -> bool) (Node (x, children)) : 'a nary_tree list
+    =
   let processed_children = List.concat_map (flatten_filter f) children in
   if f x then
     (* Node matches, keep it and its processed children as one node *)
@@ -28,7 +31,8 @@ let rec flatten_filter f (Node (x, children)) : 'a nary_tree list =
     (* Node doesn't match, flatten it by returning its children directly *)
     processed_children
 
-let remove_all_nonmatching f tree =
+let remove_all_nonmatching (f : 'a -> bool) (tree : 'a nary_tree) :
+    'a nary_tree option =
   match flatten_filter f tree with [ result ] -> Some result | _ -> None
 
 let add_child (tree : 'a nary_tree) (child : 'a nary_tree) : 'a nary_tree =
@@ -58,5 +62,11 @@ let rec top_n (n : int) (Node (value, children)) : 'a nary_tree =
 
 let rec bottom_n (n : int) (Node (_, children) as tree) =
   if n = 0 then [ tree ]
-    (* Return the current subtree when exactly at depth n *)
   else List.flatten (List.map (bottom_n (n - 1)) children)
+
+let rec depth_first_fold_with_state (f : 'acc -> 'a -> 'acc) (acc : 'acc)
+    (tree : 'a nary_tree) : 'acc =
+  match tree with
+  | Node (x, children) ->
+      let new_acc = f acc x in
+      List.fold_left (depth_first_fold f) new_acc children
