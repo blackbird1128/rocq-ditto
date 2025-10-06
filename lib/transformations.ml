f42a7d67bcbdb78824f43e353f0da26ca85afc5f
alexJ <37084688+blackbird1128@users.noreply.github.com>
2025-10-06 16:36:11 +0200

    WIP: use pattern matching instead of regex to detect assert by

--- lib/transformations.ml
+++ lib/transformations.ml
@@ -848,35 +848,132 @@ let chop_dot (x : string) : (string, Error.t) result =
          "string" x [%sexp_of: string])
   else Ok (String.sub x 0 (len - 1))
 
-let secure_node (x : syntaxNode) : (syntaxNode, Error.t) result =
-  let re =
-    Re.(
-      seq
-        [
-          str "by";
-          rep1 (char ' ');
-          group (compl [ set ".;" ] |> rep1);
-          (* group 1: content *)
-          group (set ".;");
-          (* group 2: final punctuation *)
-        ])
-    |> Re.compile
-  in
-  let new_repr =
-    Re.replace ~all:true re
-      ~f:(fun g ->
-        let content = Re.Group.get g 1 |> String.trim in
-        let ending = Re.Group.get g 2 in
-        if
-          String.length content > 0
-          && content.[0] = '('
-          && content.[String.length content - 1] = ')'
-        then "by " ^ content ^ ending
-        else "by (" ^ content ^ ")" ^ ending)
-      x.repr
-  in
+let rec first_atomic_tactic (tac : Ltac_plugin.Tacexpr.raw_tactic_expr) :
+    'a Ltac_plugin.Tacexpr.gen_atomic_tactic_expr option =
+  let open Ltac_plugin.Tacexpr in
+  match tac.CAst.v with
+  | TacAtom atom -> Some atom
+  | TacThen (t1, _) -> first_atomic_tactic t1
+  | TacThens (t1, _) -> first_atomic_tactic t1
+  | TacThens3parts (t1, _, _, _) -> first_atomic_tactic t1
+  | TacDispatch (t1 :: _) -> first_atomic_tactic t1
+  | TacFirst (t1 :: _) -> first_atomic_tactic t1
+  | TacSolve (t1 :: _) -> first_atomic_tactic t1
+  | TacTry t1
+  | TacOnce t1
+  | TacExactlyOnce t1
+  | TacRepeat t1
+  | TacProgress t1
+  | TacAbstract (t1, _)
+  | TacTime (_, t1)
+  | TacDo (_, t1)
+  | TacTimeout (_, t1)
+  | TacIfThenCatch (t1, _, _)
+  | TacOrelse (t1, _)
+  | TacOr (t1, _) ->
+      first_atomic_tactic t1
+  | TacSelect (_, t1) -> first_atomic_tactic t1
+  (* Anything else is either empty, a match, or a function *)
+  | TacId _ | TacFail _ | TacLetIn _ | TacMatch _ | TacMatchGoal _ | TacFun _
+  | TacArg _ | TacML _ | TacAlias _ | TacExtendTac _ ->
+      None
+  | TacDispatch [] | TacFirst [] | TacSolve [] -> None
 
-  Syntax_node.syntax_node_of_string new_repr x.range.start
+let get_base_tactic (x : syntaxNode) :
+    Ltac_plugin.Tacexpr.r_dispatch Ltac_plugin.Tacexpr.gen_atomic_tactic_expr
+    option =
+  let ( let* ) = Option.bind in
+  let open Raw_gen_args_converter in
+  let res =
+    let* args = Syntax_node.get_tactic_raw_generic_arguments x in
+    let* ltac = raw_arguments_to_ltac_elements args in
+    let raw_tac = ltac.raw_tactic_expr in
+
+    first_atomic_tactic raw_tac
+  in
+  res
+
+let is_node_assert_by (x : syntaxNode) : bool =
+  let ( let* ) = Option.bind in
+  let open Raw_gen_args_converter in
+  let res =
+    let* args = Syntax_node.get_tactic_raw_generic_arguments x in
+    let* ltac = raw_arguments_to_ltac_elements args in
+    let raw_tac = ltac.raw_tactic_expr in
+    match raw_tac.CAst.v with
+    | Ltac_plugin.Tacexpr.TacAtom atom -> (
+        match atom with
+        | Ltac_plugin.Tacexpr.TacAssert (_, _, by_clause_opt, _, _) -> (
+            match by_clause_opt with
+            | Some (Some _) -> Some true (* assert _ by _ *)
+            | _ -> Some false)
+        | _ -> Some false)
+    | _ -> Some false
+  in
+  res = Some true
+
+let get_ending (tac : Ltac_plugin.Tacexpr.raw_tactic_expr option) : string =
+  match tac with
+  | Some tac -> (
+      match tac.CAst.v with
+      | Ltac_plugin.Tacexpr.TacThen _ | Ltac_plugin.Tacexpr.TacThens _
+      | Ltac_plugin.Tacexpr.TacDispatch _ | Ltac_plugin.Tacexpr.TacThens3parts _
+        ->
+          ";"
+      | _ -> ".")
+  | None -> "."
+
+let secure_node (x : syntaxNode) : (syntaxNode, Error.t) result =
+  let open Raw_gen_args_converter in
+  let base_tactic = get_base_tactic x in
+  match base_tactic with
+  | Some (Ltac_plugin.Tacexpr.TacAssert (_, _, Some (Some _), _, _)) as
+    base_tactic ->
+      Logs.debug (fun m -> m "old repr: %s" x.repr);
+      let open Raw_gen_args_converter in
+      let args = Syntax_node.get_tactic_raw_generic_arguments x in
+      let ltac =
+        Option.cata Raw_gen_args_converter.raw_arguments_to_ltac_elements None
+          args
+      in
+      let raw_tac = Option.map (fun x -> x.raw_tactic_expr) ltac in
+      let ending = get_ending raw_tac in
+      let content_char = Re.alt [ Re.compl [ Re.set ".;" ]; Re.char '\n' ] in
+      let re =
+        Re.(
+          seq
+            [
+              str "by";
+              rep1 (char ' ');
+              group (rep1 content_char);
+              rep (alt [ char ' '; char '\n' ]);
+              (* allow trailing whitespace *)
+              group (set ".;");
+              (* final punctuation *)
+            ])
+        |> Re.compile
+      in
+      let new_repr =
+        Re.replace ~all:true re
+          ~f:(fun g ->
+            let content = Re.Group.get g 1 |> String.trim in
+
+            Logs.debug (fun m -> m "ending: %s " ending);
+            Logs.debug (fun m -> m "content: %s" content);
+            if
+              String.length content > 0
+              && content.[0] = '('
+              && content.[String.length content - 1] = ')'
+            then (
+              Logs.debug (fun m -> m "parenthesis detected !");
+              "by " ^ content ^ ending)
+            else "by (" ^ content ^ ")" ^ ending)
+          x.repr
+      in
+
+      Logs.debug (fun m -> m "new repr %s" new_repr);
+      Syntax_node.syntax_node_of_string new_repr x.range.start
+  | _ -> Ok x
 
 let turn_into_oneliner (doc : Coq_document.t)
     (proof_tree : syntaxNode nary_tree) :
@@ -953,6 +1050,8 @@ let turn_into_oneliner (doc : Coq_document.t)
 
           let* one_liner_repr = get_oneliner cleaned_tree in
           let one_liner_repr = one_liner_repr ^ "." in
+
+          Logs.debug (fun m -> m "one liner repr %s" one_liner_repr);
 
           let flattened = Nary_tree.flatten proof_tree in
           let remove_steps =
