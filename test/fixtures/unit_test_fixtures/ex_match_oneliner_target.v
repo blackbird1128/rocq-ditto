ea143a793d5cb32675960830eba846dfec665a1f
blackbird1128 <37084688+blackbird1128@users.noreply.github.com>
2025-10-30 13:32:12 +0100

    Update test files to match expected result: oneliner_and proof_with

--- test/fixtures/unit_test_fixtures/ex_match_oneliner_target.v
+++ test/fixtures/unit_test_fixtures/ex_match_oneliner_target.v
@@ -1,8 +1,8 @@
 Lemma foo : forall n :nat, n + 0 = n. 
 Proof.
-  induction n;
+induction n as [|n IHn];
   [ reflexivity
-   |  match goal with
-     | id:n + 0 = n |- _ => revert id
-     end].
+  | match goal with
+    | id:n + 0 = n |- _ => revert id
+    end; (simpl; (intros; (rewrite IHn; reflexivity))) ].
 Qed.
