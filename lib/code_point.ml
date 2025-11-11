cdbce017794954ca7882cab861bea05cb5bc4e09
blackbird1128 <37084688+blackbird1128@users.noreply.github.com>
2025-11-11 21:52:28 +0100

    Add to_string for code_point

--- lib/code_point.ml
+++ lib/code_point.ml
@@ -5,5 +5,7 @@ type t = { line : int; character : int } [@@deriving sexp, yojson]
 let pp (fmt : Format.formatter) (x : t) : unit =
   Format.fprintf fmt "(l: %d c: %d)" x.line x.character
 
+let to_string (x : t) : string = Format.asprintf "%a" pp x
+
 let code_point_from_lang_point (x : Lang.Point.t) : t =
   { line = x.line; character = x.character }
--- lib/code_point.mli
+++ lib/code_point.mli
@@ -1,4 +1,5 @@
 type t = { line : int; character : int } [@@deriving sexp, yojson]
 
 val pp : Format.formatter -> t -> unit
+val to_string : t -> string
 val code_point_from_lang_point : Lang.Point.t -> t
