0a5906e14b5c7d6991e1b389f7e9ba44abdf18e3
blackbird1128 <37084688+blackbird1128@users.noreply.github.com>
2025-11-24 12:12:15 +0100

    Add target for text on multiple induction hypotheses type

---
+++ test/fixtures/unit_test_fixtures/ex_explicit_induction_long_inductive_target.v
@@ -0,0 +1,27 @@
+From Stdlib Require Arith.
+
+Inductive light : Type :=
+  | Red
+  | Green
+  | Blue
+  | Blink (a b : light).
+
+Fixpoint count (l : light) : nat :=
+  match l with
+  | Red => 1
+  | Green => 1
+  | Blue => 1
+  | Blink a b => count a + count b
+  end.
+
+Theorem count_positive : forall l, count l > 0.
+Proof.
+ intros.
+ induction l as [ | | | l1 IHl1 l2 IHl2 ].
+  - auto with arith.
+  - auto with arith.
+  - auto with arith.
+  - auto with arith.
+    simpl;
+    auto with arith.
+Qed.
