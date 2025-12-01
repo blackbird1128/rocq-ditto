a35b87487174858f555e3707ed306dde0612b04c
alexJ <37084688+blackbird1128@users.noreply.github.com>
2025-12-01 16:51:14 +0100

    Add the possibility to reuse a local switch (needed for CI caching)

--- configure.sh
+++ configure.sh
@@ -20,31 +20,40 @@ fi
 
 VERSION="$1"
 
-if [[ "$VERSION" == "latest" ]]; then
-  echo ">> Setting up with the latest Rocq version..."
-  opam switch -y create . ocaml-base-compiler --deps-only
-#  opam install -y rocq-stdlib
+if [[ -d _opam ]]; then
+  echo ">> Reusing local opam switch"
   eval "$(opam env)"
-  mkdir -p vendor/
-  ln -f  _opam/bin/rocq _opam/bin/coqc
-  cp ./_opam/bin/fcc ./vendor/fcc
-  make build
+  REUSE_SWITCH=1
 else
-  echo ">> Setting up with Rocq version $VERSION..."
-  opam switch create . ocaml-base-compiler.5.4.0 --no-install
-  # Pin either rocq-core (>=9.0.0) or coq-core (<9.0.0)
-
-  PKG="$(detect_pkg "$VERSION")"
-  echo ">> Pinning $PKG $VERSION..."
-  opam pin add -y "$PKG" "$VERSION"
-  
-  opam install -y . --deps-only
-  # For pinned Rocq version, dune still expects coqc to exist
-  if [[ -f _opam/bin/rocq ]]; then
-      ln -f _opam/bin/rocq _opam/bin/coqc
-  fi
-  mkdir -p vendor/
-  cp ./_opam/bin/fcc ./vendor/fcc
+  REUSE_SWITCH=0
 fi
+
+if [[ $REUSE_SWITCH -eq 0 ]]; then
+    if [[ "$VERSION" == "latest" ]]; then
+	echo ">> Setting up with the latest Rocq version..."
+	opam switch -y create . ocaml-base-compiler --deps-only
+	#  opam install -y rocq-stdlib
+	eval "$(opam env)"
+    else
+	echo ">> Setting up with Rocq version $VERSION..."
+	opam switch create . ocaml-base-compiler.5.4.0 --no-install
+	# Pin either rocq-core (>=9.0.0) or coq-core (<9.0.0)
+
+	PKG="$(detect_pkg "$VERSION")"
+	echo ">> Pinning $PKG $VERSION..."
+	opam pin add -y "$PKG" "$VERSION"
+  
+	opam install -y . --deps-only
+	# For pinned Rocq version, dune still expects coqc to exist
+
+    fi
+fi
+
+if [[ -f _opam/bin/rocq ]]; then
+    ln -f _opam/bin/rocq _opam/bin/coqc
+fi
+mkdir -p vendor/
+cp ./_opam/bin/fcc ./vendor/fcc
+
 
 echo "Configuration completed successfully!"
