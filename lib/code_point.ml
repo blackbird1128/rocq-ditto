1bff6e923e13deccafeb9becaf2a9c88612e86c0
blackbird1128 <37084688+blackbird1128@users.noreply.github.com>
2026-03-05 20:20:52 +0100

    Add compare and leq to code_point

--- lib/code_point.ml
+++ lib/code_point.ml
@@ -5,6 +5,11 @@ type t = { line : int; character : int } [@@deriving sexp, yojson]
 let pp (fmt : Format.formatter) (x : t) : unit =
   Format.fprintf fmt "(l: %d c: %d)" x.line x.character
 
+let compare (a : t) (b : t) : int =
+  let c = compare a.line b.line in
+  if c <> 0 then c else compare a.character b.character
+
+let leq (a : t) (b : t) : bool = compare a b <= 0
 let to_string (x : t) : string = Format.asprintf "%a" pp x
 
 let shift (n_line : int) (n_char : int) (x : t) : t =
--- lib/code_point.mli
+++ lib/code_point.mli
@@ -1,6 +1,8 @@
 type t = { line : int; character : int } [@@deriving sexp, yojson]
 
 val pp : Format.formatter -> t -> unit
+val compare : t -> t -> int
+val leq : t -> t -> bool
 val shift : int -> int -> t -> t
 val to_string : t -> string
 val code_point_from_lang_point : Lang.Point.t -> t
