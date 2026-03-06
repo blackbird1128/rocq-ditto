50d1a4c23b8fbdef6f14a30b5615961eef5a42f3
alexJ <37084688+blackbird1128@users.noreply.github.com>
2026-03-06 17:45:38 +0100

    Complete blacklist up to ch07_midpoint included

--- tools/constructivisation_data_generator_plugin/main.ml
+++ tools/constructivisation_data_generator_plugin/main.ml
@@ -1,20 +1,17 @@
 open Fleche
 open Ditto
 
-let write_ocaml_list (out : out_channel) (name : string) (items : string list)
-    =
+let write_ocaml_list (out : out_channel) (name : string) (items : string list) =
   output_string out ("let " ^ name ^ " =\n  [\n");
-  List.iter
-    (fun item -> output_string out ("    \"" ^ item ^ "\";\n"))
-    items;
+  List.iter (fun item -> output_string out ("    \"" ^ item ^ "\";\n")) items;
   output_string out "  ]\n\n"
 
 let generate_data_file ~io ~token:_ ~(doc : Doc.t) =
   let uri = doc.uri in
   let uri_str = Lang.LUri.File.to_string_uri uri in
   let lvl = Io.Level.Info in
-  Io.Report.msg ~io ~lvl
-    "[constructivisation-data-generator] processing %s ..." uri_str;
+  Io.Report.msg ~io ~lvl "[constructivisation-data-generator] processing %s ..."
+    uri_str;
 
   let parsed_document = Rocq_document.parse_document doc in
   let definitions =
@@ -36,7 +33,59 @@ let generate_data_file ~io ~token:_ ~(doc : Doc.t) =
     "(* This file is generated. Do not edit by hand. *)\n\n";
   write_ocaml_list out_chan "definitions_with_exists" definitions_with_exists;
   write_ocaml_list out_chan "decidability_lemmas"
-    [ "eq_dec_points"; "cong_dec"; "bet_dec"; "col_dec" ];
+    [
+      "eq_dec_points";
+      "cong_dec";
+      "bet_dec";
+      "col_dec";
+      "out_dec";
+      "midpoint_dec";
+    ];
+  write_ocaml_list out_chan "blacklisted_proofs"
+    [
+      "Col_cases";
+      "l4_13";
+      "l4_16";
+      "NCol_cases";
+      "l5_1";
+      "l5_2";
+      "bet3__bet";
+      "between_cong_3";
+      "lt_diff";
+      "fourth_point";
+      "third_point";
+      "l5_12_b";
+      "l6_2";
+      "bet_out__bet";
+      "l6_3_2";
+      "l6_4_2";
+      "l6_7";
+      "out2_bet_out";
+      "l6_16_1";
+      "out2__bet";
+      "l6_11_uniqueness";
+      "or_bet_out";
+      "not_bet_out";
+      "out_to_bet";
+      "col_out2_col";
+      "bet2_out_out";
+      "out_bet_out_1";
+      "out_bet__out";
+      "Out_cases";
+      "Mid_cases";
+      "l7_20";
+      "l7_20_bis";
+      "cong_col_mid";
+      "l7_22_aux";
+      "l7_22";
+      "midpoint_out";
+      "midpoint_preserves_out";
+      "col_cong_bet";
+      "col_cong2_bet1";
+      "col_cong2_bet2";
+      "col_cong2_bet3";
+      "col_cong2_bet4";
+    ];
   close_out out_chan
 
 let main () = Theory.Register.Completed.add generate_data_file
