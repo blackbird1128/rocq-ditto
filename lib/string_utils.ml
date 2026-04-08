eaa8631adad3a49bb87b30555b3f1f5fe6adaaca
blackbird1128 <37084688+blackbird1128@users.noreply.github.com>
2026-04-08 22:30:56 +0200

    Add remove_prefix to string utils

--- lib/string_utils.ml
+++ lib/string_utils.ml
@@ -3,3 +3,9 @@ let split_prefix (prefix : string) (s : string) : (string * string) option =
   if String.length s >= plen && String.sub s 0 plen = prefix then
     Some (prefix, String.sub s plen (String.length s - plen))
   else None
+
+let remove_prefix (str : string) (prefix : string) =
+  let prefix_len = String.length prefix in
+  if String.length str >= prefix_len && String.sub str 0 prefix_len = prefix
+  then String.sub str prefix_len (String.length str - prefix_len)
+  else str
--- lib/string_utils.mli
+++ lib/string_utils.mli
@@ -1,1 +1,2 @@
 val split_prefix : string -> string -> (string * string) option
+val remove_prefix : string -> string -> string
