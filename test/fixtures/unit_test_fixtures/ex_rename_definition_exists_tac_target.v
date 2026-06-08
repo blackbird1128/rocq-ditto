e66fa138d04f408bfa73a6bff8979d2a984151bd
alexJ <37084688+blackbird1128@users.noreply.github.com>
2026-06-08 17:22:35 +0200

    Fix spacing issue in test rename definition exists target

--- test/fixtures/unit_test_fixtures/ex_rename_definition_exists_tac_target.v
+++ test/fixtures/unit_test_fixtures/ex_rename_definition_exists_tac_target.v
@@ -2,7 +2,7 @@ Definition Bar A B C := A \/ B \/ C.
 
 Definition Feed A1 B1 C1 := Bar A1 B1 C1 \/ Bar C1 B1 A1.
 
-Lemma foo_feed : forall A B C, A \/ B \/ C -> exists D E F, Feed D E F.
+Lemma foo_feed : forall A B C, A \/ B \/ C ->  exists D E F, Feed D E F.
 Proof.
   intros.
   repeat exists (Bar A B C).
