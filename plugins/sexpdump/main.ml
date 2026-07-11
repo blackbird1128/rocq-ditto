06a60fcbd4620563434baf6b421004a2ccd697f4
blackbird1128 <37084688+blackbird1128@users.noreply.github.com>
2026-07-12 00:34:10 +0200

    Add a way to remove location from nodes in s-exp dump plugin

--- plugins/sexpdump/main.ml
+++ plugins/sexpdump/main.ml
@@ -3,8 +3,18 @@ open Fleche
 
 let pp_sexp fmt obj = Format.fprintf fmt "@[%a@\n@]" Sexplib.Sexp.pp_hum obj
 
+let rec strip_loc_field (sexp : Sexplib.Sexp.t) : Sexplib.Sexp.t option =
+  match sexp with
+  | Atom _ as atom -> Some atom
+  | List (Atom "loc" :: _) -> None
+  | List elements -> Some (List (List.filter_map strip_loc_field elements))
+
+let strip_loc (sexp : Sexplib.Sexp.t) : Sexplib.Sexp.t =
+  match strip_loc_field sexp with Some sexp -> sexp | None -> List []
+
 let pp_ast_sexp fmt (ast : Doc.Node.Ast.t) =
   Serlib.Ser_vernacexpr.sexp_of_vernac_control (Coq.Ast.to_coq ast.v)
+  |> (if Sys.getenv_opt "SEXPDUMP_STRIP_LOCS" = None then Fun.id else strip_loc)
   |> pp_sexp fmt
 
 let pw pp fmt ast = Format.fprintf fmt "@[%a@]" pp ast
