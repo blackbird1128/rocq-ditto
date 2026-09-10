ea366b314abad2ceb1823bf0b70cc1b465e728b3
blackbird1128 <37084688+blackbird1128@users.noreply.github.com>
2026-09-10 13:08:19 +0200

    Test advance_by_text properties

--- test/test_point.ml
+++ test/test_point.ml
@@ -99,6 +99,73 @@ let test_roundtrip_parsing_sexp_prop =
       | Ok of_sexp -> equal of_sexp point
       | Error _ -> false)
 
+let test_advance_by_text_simple_no_newline () =
+  let text = "aaaa" in
+  let start_point = origin in
+
+  let expected = point ~line:0 ~char:4 in
+
+  check point_testable "the starting point should be moved by 4 chars" expected
+    (advance_by_text start_point text)
+
+let test_advance_by_text_simple_one_newline () =
+  let text = "aaaa\naa" in
+  let start_point = origin in
+
+  let expected = point ~line:1 ~char:2 in
+
+  check point_testable "the point should advance to the expected point" expected
+    (advance_by_text start_point text)
+
+let test_advance_by_empty_string_is_identity_prop =
+  QCheck.Test.make ~count:1000
+    ~name:
+      "Advancing a point by an empty string should leave the point at the \
+       start position"
+    QCheck.(pair int_pos int_pos)
+    (fun (line, char) ->
+      let point = point ~line ~char in
+
+      let moved = advance_by_text point "" in
+      equal point moved)
+
+let test_advance_by_number_of_newline_in_text_prop =
+  QCheck.Test.make ~count:1000
+    ~name:
+      "Advancing a point by a string should increase the line count by the \
+       number of newlines (\n\
+       )"
+    QCheck.(pair (pair int_pos int_pos) string_printable)
+    (fun ((line, char), text) ->
+      let start_point = point ~line ~char in
+
+      let number_newline =
+        String.fold_left
+          (fun count char -> if char = '\n' then count + 1 else count)
+          0 text
+      in
+      let expected =
+        shift ~lines:number_newline ~chars:0 start_point
+        |> expect_result_ok ~context:"shift by a positive number"
+      in
+
+      let moved = advance_by_text start_point text in
+
+      expected.line = moved.line)
+
+let test_advance_without_newline_only_move_char_prop =
+  QCheck.Test.make ~count:1000
+    ~name:
+      "Advancing a point by a string without newlines should only move the \
+       number of chars"
+    QCheck.(pair (pair int_pos int_pos) string_printable)
+    (fun ((line, char), text) ->
+      QCheck.assume (not (String.exists (fun c -> c = '\n') text));
+      let start_point = point ~line ~char in
+
+      let moved = advance_by_text start_point text in
+      start_point.line = moved.line)
+
 let () =
   let qcheck_tests_parsing_json =
     List.map QCheck_alcotest.to_alcotest [ test_roundtrip_parsing_json_prop ]
@@ -106,6 +173,15 @@ let () =
 
   let qcheck_tests_parsing_sexp =
     List.map QCheck_alcotest.to_alcotest [ test_roundtrip_parsing_sexp_prop ]
+  in
+
+  let qcheck_point_functions_property_tests =
+    List.map QCheck_alcotest.to_alcotest
+      [
+        test_advance_by_empty_string_is_identity_prop;
+        test_advance_by_number_of_newline_in_text_prop;
+        test_advance_without_newline_only_move_char_prop;
+      ]
   in
 
   run "Point module tests"
@@ -138,4 +214,14 @@ let () =
             `Quick test_of_sexp_simple;
         ]
         @ qcheck_tests_parsing_sexp );
+      ( "Point functions",
+        [
+          test_case "test advancing a point by a string of four chars" `Quick
+            test_advance_by_text_simple_no_newline;
+          test_case
+            "test advancing a point by a string with a single newline in the \
+             middle"
+            `Quick test_advance_by_text_simple_one_newline;
+        ]
+        @ qcheck_point_functions_property_tests );
     ]
